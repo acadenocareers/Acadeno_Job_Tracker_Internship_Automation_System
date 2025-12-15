2f87f71c4645545b24629d181677278b59a32666
acadenocareers <acadenocareers@gmail.com>
2025-12-15 11:02:48 +0530

    last

--- appComb.py
+++ appComb.py
@@ -435,4 +435,5 @@ def send_job_poster_route():
 # =========================================================
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
